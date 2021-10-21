c2352be8884c1dd1b73e67af3da0dbca1dc97d8e
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2021-10-21 19:36:22 +0200

    nuevo diseño

--- ejercicio02_PerezSara/ejercicio02_PerezSara/Form1.Designer.vb
+++ ejercicio02_PerezSara/ejercicio02_PerezSara/Form1.Designer.vb
@@ -27,12 +27,17 @@ Partial Class Form1
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.txtBPassword = New System.Windows.Forms.TextBox()
         Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(97, 50)
+        Me.Label1.Location = New System.Drawing.Point(23, 62)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(58, 17)
         Me.Label1.TabIndex = 0
@@ -41,7 +46,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(74, 113)
+        Me.Label2.Location = New System.Drawing.Point(23, 122)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(81, 17)
         Me.Label2.TabIndex = 1
@@ -49,14 +54,14 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(161, 50)
+        Me.TextBox1.Location = New System.Drawing.Point(110, 59)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(100, 22)
         Me.TextBox1.TabIndex = 2
         '
         'txtBPassword
         '
-        Me.txtBPassword.Location = New System.Drawing.Point(161, 108)
+        Me.txtBPassword.Location = New System.Drawing.Point(110, 117)
         Me.txtBPassword.Name = "txtBPassword"
         Me.txtBPassword.Size = New System.Drawing.Size(100, 22)
         Me.txtBPassword.TabIndex = 3
@@ -64,16 +69,55 @@ Partial Class Form1
         'DateTimePicker1
         '
         Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.DateTimePicker1.Location = New System.Drawing.Point(77, 198)
+        Me.DateTimePicker1.Location = New System.Drawing.Point(26, 178)
         Me.DateTimePicker1.Name = "DateTimePicker1"
-        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 22)
+        Me.DateTimePicker1.Size = New System.Drawing.Size(184, 22)
         Me.DateTimePicker1.TabIndex = 4
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(236, 59)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(110, 105)
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(236, 170)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(110, 32)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Cargar Imagen"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(98, 244)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 37)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Submit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(204, 244)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 37)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Salir"
+        Me.Button3.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(382, 353)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.DateTimePicker1)
         Me.Controls.Add(Me.txtBPassword)
         Me.Controls.Add(Me.TextBox1)
@@ -82,6 +126,7 @@ Partial Class Form1
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -92,4 +137,8 @@ Partial Class Form1
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents txtBPassword As TextBox
     Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
