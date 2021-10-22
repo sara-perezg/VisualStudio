01d4268b52ff32b7023a2ef23d96485c6b00d9fc
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2021-10-22 19:09:10 +0200

    name and password mandatory fields

--- ejercicio02_PerezSara/ejercicio02_PerezSara/Form1.Designer.vb
+++ ejercicio02_PerezSara/ejercicio02_PerezSara/Form1.Designer.vb
@@ -22,123 +22,125 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.txtBName = New System.Windows.Forms.TextBox()
         Me.txtBPassword = New System.Windows.Forms.TextBox()
-        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.dtpDate = New System.Windows.Forms.DateTimePicker()
+        Me.pbImage = New System.Windows.Forms.PictureBox()
+        Me.btnLoadImage = New System.Windows.Forms.Button()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.pbImage, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Label1
+        'lblName
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(23, 62)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(58, 17)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Nombre"
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(23, 62)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(58, 17)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Nombre"
         '
-        'Label2
+        'lblPassword
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(23, 122)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(81, 17)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Contraseña"
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Location = New System.Drawing.Point(23, 122)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(81, 17)
+        Me.lblPassword.TabIndex = 1
+        Me.lblPassword.Text = "Contraseña"
         '
-        'TextBox1
+        'txtBName
         '
-        Me.TextBox1.Location = New System.Drawing.Point(110, 59)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
-        Me.TextBox1.TabIndex = 2
+        Me.txtBName.Location = New System.Drawing.Point(110, 59)
+        Me.txtBName.Name = "txtBName"
+        Me.txtBName.Size = New System.Drawing.Size(100, 22)
+        Me.txtBName.TabIndex = 2
         '
         'txtBPassword
         '
         Me.txtBPassword.Location = New System.Drawing.Point(110, 117)
+        Me.txtBPassword.MaxLength = 6
         Me.txtBPassword.Name = "txtBPassword"
+        Me.txtBPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
         Me.txtBPassword.Size = New System.Drawing.Size(100, 22)
         Me.txtBPassword.TabIndex = 3
         '
-        'DateTimePicker1
+        'dtpDate
         '
-        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.DateTimePicker1.Location = New System.Drawing.Point(26, 178)
-        Me.DateTimePicker1.Name = "DateTimePicker1"
-        Me.DateTimePicker1.Size = New System.Drawing.Size(184, 22)
-        Me.DateTimePicker1.TabIndex = 4
+        Me.dtpDate.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpDate.Location = New System.Drawing.Point(26, 178)
+        Me.dtpDate.Name = "dtpDate"
+        Me.dtpDate.Size = New System.Drawing.Size(184, 22)
+        Me.dtpDate.TabIndex = 4
         '
-        'PictureBox1
+        'pbImage
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(236, 59)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(110, 105)
-        Me.PictureBox1.TabIndex = 5
-        Me.PictureBox1.TabStop = False
+        Me.pbImage.Location = New System.Drawing.Point(236, 59)
+        Me.pbImage.Name = "pbImage"
+        Me.pbImage.Size = New System.Drawing.Size(110, 105)
+        Me.pbImage.TabIndex = 5
+        Me.pbImage.TabStop = False
         '
-        'Button1
+        'btnLoadImage
         '
-        Me.Button1.Location = New System.Drawing.Point(236, 170)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(110, 32)
-        Me.Button1.TabIndex = 6
-        Me.Button1.Text = "Cargar Imagen"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnLoadImage.Location = New System.Drawing.Point(236, 170)
+        Me.btnLoadImage.Name = "btnLoadImage"
+        Me.btnLoadImage.Size = New System.Drawing.Size(110, 45)
+        Me.btnLoadImage.TabIndex = 6
+        Me.btnLoadImage.Text = "Cargar Imagen"
+        Me.btnLoadImage.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btnSubmit
         '
-        Me.Button2.Location = New System.Drawing.Point(98, 244)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 37)
-        Me.Button2.TabIndex = 7
-        Me.Button2.Text = "Submit"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnSubmit.Location = New System.Drawing.Point(98, 244)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 37)
+        Me.btnSubmit.TabIndex = 7
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
         '
-        'Button3
+        'btnExit
         '
-        Me.Button3.Location = New System.Drawing.Point(204, 244)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 37)
-        Me.Button3.TabIndex = 8
-        Me.Button3.Text = "Salir"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btnExit.Location = New System.Drawing.Point(204, 244)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 37)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Salir"
+        Me.btnExit.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(382, 353)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.PictureBox1)
-        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.btnLoadImage)
+        Me.Controls.Add(Me.pbImage)
+        Me.Controls.Add(Me.dtpDate)
         Me.Controls.Add(Me.txtBPassword)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtBName)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.lblName)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.Name = "Form1"
-        Me.Text = "Form1"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Text = "Profile"
+        CType(Me.pbImage, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblPassword As Label
+    Friend WithEvents txtBName As TextBox
     Friend WithEvents txtBPassword As TextBox
-    Friend WithEvents DateTimePicker1 As DateTimePicker
-    Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
+    Friend WithEvents dtpDate As DateTimePicker
+    Friend WithEvents pbImage As PictureBox
+    Friend WithEvents btnLoadImage As Button
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents btnExit As Button
 End Class
--- ejercicio02_PerezSara/ejercicio02_PerezSara/Form1.vb
+++ ejercicio02_PerezSara/ejercicio02_PerezSara/Form1.vb
@@ -3,5 +3,12 @@
         txtBPassword.PasswordChar = "*"
     End Sub
 
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        If txtBPassword.Text = "" Or txtBName.Text = "" Then
+            MsgBox("Tiene que rellenar todos los campos")
+        Else
+            MsgBox("Perfil actualizado correctamente!")
+        End If
 
+    End Sub
 End Class
