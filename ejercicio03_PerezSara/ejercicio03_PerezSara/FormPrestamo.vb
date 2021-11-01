c80b1de60b623ee56694564c917a9b3780eb983a
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2021-11-01 16:07:35 +0100

    connecting volver buttons and exits

--- ejercicio03_PerezSara/ejercicio03_PerezSara/FormPrestamo.vb
+++ ejercicio03_PerezSara/ejercicio03_PerezSara/FormPrestamo.vb
@@ -3,4 +3,14 @@
         lblName.Text = FormInicio.txtBName.Text
 
     End Sub
+
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        Me.Hide()
+        FormInicio.Show()
+    End Sub
+
+    Private Sub btnConSolicitud_Click(sender As Object, e As EventArgs) Handles btnConSolicitud.Click
+        Me.Hide()
+        FormSolucion.Show()
+    End Sub
 End Class
--- ejercicio03_PerezSara/ejercicio03_PerezSara/FormSolucion.vb
+++ ejercicio03_PerezSara/ejercicio03_PerezSara/FormSolucion.vb
@@ -1,3 +1,10 @@
 ﻿Public Class FormSolucion
+    Private Sub btnVolver_Click(sender As Object, e As EventArgs) Handles btnVolver.Click
+        Me.Hide()
+        FormPrestamo.Show()
+    End Sub
 
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Close()
+    End Sub
 End Class
