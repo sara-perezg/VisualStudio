5f619b300138a731471370e283b67169cb7ae09a
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2021-11-12 17:40:47 +0100

    cleaning fInicio class

--- ejercicio05_PerezSara/ejercicio05_PerezSara/fInicio.vb
+++ ejercicio05_PerezSara/ejercicio05_PerezSara/fInicio.vb
@@ -1,9 +1,3 @@
 ﻿Public Class fInicio
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
-    End Sub
-
-    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
-
-    End Sub
 End Class
