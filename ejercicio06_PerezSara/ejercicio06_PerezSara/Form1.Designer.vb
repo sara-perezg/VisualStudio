fe81b19192ebc8190aa7f87ed048931115cfa390
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2021-11-25 20:25:24 +0100

    new ej06

---
+++ ejercicio06_PerezSara/ejercicio06_PerezSara/Form1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnExportar = New System.Windows.Forms.Button()
+        Me.btnImportar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.ejercicio06_PerezSara.My.Resources.Resources.img4
+        Me.PictureBox1.Location = New System.Drawing.Point(488, 75)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(237, 277)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(80, 94)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(194, 22)
+        Me.txtNombre.TabIndex = 1
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(330, 88)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(79, 35)
+        Me.btnAdd.TabIndex = 2
+        Me.btnAdd.Text = "Añadir"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnExportar
+        '
+        Me.btnExportar.Location = New System.Drawing.Point(95, 378)
+        Me.btnExportar.Name = "btnExportar"
+        Me.btnExportar.Size = New System.Drawing.Size(79, 35)
+        Me.btnExportar.TabIndex = 3
+        Me.btnExportar.Text = "Exportar"
+        Me.btnExportar.UseVisualStyleBackColor = True
+        '
+        'btnImportar
+        '
+        Me.btnImportar.Location = New System.Drawing.Point(330, 378)
+        Me.btnImportar.Name = "btnImportar"
+        Me.btnImportar.Size = New System.Drawing.Size(79, 35)
+        Me.btnImportar.TabIndex = 4
+        Me.btnImportar.Text = "Importar"
+        Me.btnImportar.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(565, 378)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(79, 35)
+        Me.btnSalir.TabIndex = 5
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(266, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(214, 32)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Lista Películas"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Items.AddRange(New Object() {"Star Wars 1", "Star Wars 2", "Pulp Fiction", "Motel"})
+        Me.ListBox1.Location = New System.Drawing.Point(80, 160)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(210, 164)
+        Me.ListBox1.TabIndex = 7
+        '
+        'frmInicio
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnImportar)
+        Me.Controls.Add(Me.btnExportar)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "frmInicio"
+        Me.Text = "VideoClub"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnExportar As Button
+    Friend WithEvents btnImportar As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ ejercicio06_PerezSara/ejercicio06_PerezSara/Form1.vb
@@ -0,0 +1,86 @@
+﻿Public Class Form1
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+        ListBox1.Items.Add(txtNombre.Text)
+        txtNombre.Text = ""
+        txtNombre.Focus()
+
+    End Sub
+
+    Private Sub btnExportar_Click(sender As Object, e As EventArgs) Handles btnExportar.Click
+
+        ' Declaramos la variable guardar como nuevo cuadro de diálogo
+        ' La variable guardar va a ser un archivo de texto
+        ' Condición, utilzamos el sistema windows para realizar el cuadro de diálogo de guardar.
+        ' En la variable sw se guardará lo que escribamos y con for each guardamos cada línea de texto en en el txt
+
+        Dim guardar As New SaveFileDialog
+        guardar.Filter = "Archivo Text | *.txt"
+        If guardar.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Dim sw As New System.IO.StreamWriter(guardar.FileName)
+
+            For Each item In ListBox1.Items
+                sw.WriteLine(item.ToString())
+            Next
+
+            sw.Close()
+        End If
+
+        Dim respuesta As MsgBoxResult
+        respuesta = MessageBox.Show("¿Desea limpiar la lista", "Nota", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If respuesta = vbYes Then
+            ListBox1.Items.Clear()
+        End If
+    End Sub
+
+    Private Sub btnImportar_Click(sender As Object, e As EventArgs) Handles btnImportar.Click
+        Dim ruta As String = ""
+        Dim abrirFD As New OpenFileDialog()
+        With abrirFD
+            .Title = "Seleccionar archivos"
+            .Filter = "Archivos Text(*.Txt*.txt)|*.Txt;*txt|Todos los archivos(*.*)|*.*"
+            .Multiselect = False
+            .InitialDirectory = My.Computer.FileSystem.SpecialDirectories.Desktop
+            If .ShowDialog = Windows.Forms.DialogResult.OK Then
+                ruta = .FileName
+            End If
+        End With
+        Dim sr As New System.IO.StreamReader(ruta)
+        While Not sr.EndOfStream
+            ListBox1.Items.Add(sr.ReadLine())
+        End While
+        sr.Close()
+
+
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+    End Sub
+
+    'Es opcional
+    Private Sub txtNombre_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNombre.KeyPress
+        If Asc(e.KeyChar) = 13 Then
+            Me.btnAdd.PerformClick()
+        End If
+    End Sub
+
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+        Dim I As String
+        I = ListBox1.SelectedItem
+        If ListBox1.SelectedItem = "Star Wars 1" Then
+            PictureBox1.Image = My.Resources.img1
+        End If
+        If ListBox1.SelectedItem = "Star Wars 2" Then
+            PictureBox1.Image = My.Resources.img4
+        End If
+        If ListBox1.SelectedItem = "Pulp Fiction" Then
+            PictureBox1.Image = My.Resources.img2
+        End If
+        If ListBox1.SelectedItem = "Motel" Then
+            PictureBox1.Image = My.Resources.img3
+        End If
+    End Sub
+End Class
