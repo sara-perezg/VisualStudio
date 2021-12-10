e3a291d80d8a580815b32fd59ae4dfa202510b1c
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2021-12-10 17:50:44 +0100

    Update Form1.Designer.vb
    label names

--- Ejercicio02-01PerezSara/Ejercicio02-01PerezSara/Form1.Designer.vb
+++ Ejercicio02-01PerezSara/Ejercicio02-01PerezSara/Form1.Designer.vb
@@ -55,17 +55,17 @@ Partial Class Form1
         Me.GroupBox1.Size = New System.Drawing.Size(211, 277)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "GroupBox1"
+        Me.GroupBox1.Text = "Clase Trabajador"
         '
         'RadioButton4
         '
         Me.RadioButton4.AutoSize = True
         Me.RadioButton4.Location = New System.Drawing.Point(32, 193)
         Me.RadioButton4.Name = "RadioButton4"
-        Me.RadioButton4.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton4.Size = New System.Drawing.Size(100, 21)
         Me.RadioButton4.TabIndex = 3
         Me.RadioButton4.TabStop = True
-        Me.RadioButton4.Text = "RadioButton4"
+        Me.RadioButton4.Text = "Profesional"
         Me.RadioButton4.UseVisualStyleBackColor = True
         '
         'RadioButton3
@@ -73,10 +73,10 @@ Partial Class Form1
         Me.RadioButton3.AutoSize = True
         Me.RadioButton3.Location = New System.Drawing.Point(32, 147)
         Me.RadioButton3.Name = "RadioButton3"
-        Me.RadioButton3.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton3.Size = New System.Drawing.Size(79, 21)
         Me.RadioButton3.TabIndex = 2
         Me.RadioButton3.TabStop = True
-        Me.RadioButton3.Text = "RadioButton3"
+        Me.RadioButton3.Text = "Tecnico"
         Me.RadioButton3.UseVisualStyleBackColor = True
         '
         'RadioButton2
@@ -84,10 +84,10 @@ Partial Class Form1
         Me.RadioButton2.AutoSize = True
         Me.RadioButton2.Location = New System.Drawing.Point(32, 101)
         Me.RadioButton2.Name = "RadioButton2"
-        Me.RadioButton2.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton2.Size = New System.Drawing.Size(117, 21)
         Me.RadioButton2.TabIndex = 1
         Me.RadioButton2.TabStop = True
-        Me.RadioButton2.Text = "RadioButton2"
+        Me.RadioButton2.Text = "Administrativo"
         Me.RadioButton2.UseVisualStyleBackColor = True
         '
         'RadioButton1
@@ -95,10 +95,10 @@ Partial Class Form1
         Me.RadioButton1.AutoSize = True
         Me.RadioButton1.Location = New System.Drawing.Point(32, 55)
         Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton1.Size = New System.Drawing.Size(74, 21)
         Me.RadioButton1.TabIndex = 0
         Me.RadioButton1.TabStop = True
-        Me.RadioButton1.Text = "RadioButton1"
+        Me.RadioButton1.Text = "Obrero"
         Me.RadioButton1.UseVisualStyleBackColor = True
         '
         'GroupBox2
@@ -111,17 +111,17 @@ Partial Class Form1
         Me.GroupBox2.Size = New System.Drawing.Size(193, 277)
         Me.GroupBox2.TabIndex = 1
         Me.GroupBox2.TabStop = False
-        Me.GroupBox2.Text = "GroupBox2"
+        Me.GroupBox2.Text = "Años de Servicio"
         '
         'RadioButton9
         '
         Me.RadioButton9.AutoSize = True
         Me.RadioButton9.Location = New System.Drawing.Point(37, 193)
         Me.RadioButton9.Name = "RadioButton9"
-        Me.RadioButton9.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton9.Size = New System.Drawing.Size(130, 21)
         Me.RadioButton9.TabIndex = 7
         Me.RadioButton9.TabStop = True
-        Me.RadioButton9.Text = "RadioButton9"
+        Me.RadioButton9.Text = "Más de 10 años"
         Me.RadioButton9.UseVisualStyleBackColor = True
         '
         'RadioButton8
@@ -129,10 +129,10 @@ Partial Class Form1
         Me.RadioButton8.AutoSize = True
         Me.RadioButton8.Location = New System.Drawing.Point(37, 124)
         Me.RadioButton8.Name = "RadioButton8"
-        Me.RadioButton8.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton8.Size = New System.Drawing.Size(141, 21)
         Me.RadioButton8.TabIndex = 6
         Me.RadioButton8.TabStop = True
-        Me.RadioButton8.Text = "RadioButton8"
+        Me.RadioButton8.Text = "Entre 5 y 10 años"
         Me.RadioButton8.UseVisualStyleBackColor = True
         '
         'RadioButton7
@@ -140,10 +140,10 @@ Partial Class Form1
         Me.RadioButton7.AutoSize = True
         Me.RadioButton7.Location = New System.Drawing.Point(37, 55)
         Me.RadioButton7.Name = "RadioButton7"
-        Me.RadioButton7.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton7.Size = New System.Drawing.Size(122, 21)
         Me.RadioButton7.TabIndex = 5
         Me.RadioButton7.TabStop = True
-        Me.RadioButton7.Text = "RadioButton7"
+        Me.RadioButton7.Text = "Más de 5 años"
         Me.RadioButton7.UseVisualStyleBackColor = True
         '
         'GroupBox3
@@ -155,17 +155,17 @@ Partial Class Form1
         Me.GroupBox3.Size = New System.Drawing.Size(226, 277)
         Me.GroupBox3.TabIndex = 1
         Me.GroupBox3.TabStop = False
-        Me.GroupBox3.Text = "GroupBox3"
+        Me.GroupBox3.Text = "Inscrito en un Sindicato"
         '
         'RadioButton6
         '
         Me.RadioButton6.AutoSize = True
         Me.RadioButton6.Location = New System.Drawing.Point(56, 147)
         Me.RadioButton6.Name = "RadioButton6"
-        Me.RadioButton6.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton6.Size = New System.Drawing.Size(47, 21)
         Me.RadioButton6.TabIndex = 5
         Me.RadioButton6.TabStop = True
-        Me.RadioButton6.Text = "RadioButton6"
+        Me.RadioButton6.Text = "No"
         Me.RadioButton6.UseVisualStyleBackColor = True
         '
         'RadioButton5
@@ -173,24 +173,24 @@ Partial Class Form1
         Me.RadioButton5.AutoSize = True
         Me.RadioButton5.Location = New System.Drawing.Point(56, 80)
         Me.RadioButton5.Name = "RadioButton5"
-        Me.RadioButton5.Size = New System.Drawing.Size(115, 21)
+        Me.RadioButton5.Size = New System.Drawing.Size(41, 21)
         Me.RadioButton5.TabIndex = 4
         Me.RadioButton5.TabStop = True
-        Me.RadioButton5.Text = "RadioButton5"
+        Me.RadioButton5.Text = "Si"
         Me.RadioButton5.UseVisualStyleBackColor = True
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(73, 372)
+        Me.Label1.Location = New System.Drawing.Point(65, 375)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(51, 17)
+        Me.Label1.Size = New System.Drawing.Size(111, 17)
         Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "Numero de hijos"
         '
         'DomainUpDown1
         '
-        Me.DomainUpDown1.Location = New System.Drawing.Point(131, 369)
+        Me.DomainUpDown1.Location = New System.Drawing.Point(190, 372)
         Me.DomainUpDown1.Name = "DomainUpDown1"
         Me.DomainUpDown1.Size = New System.Drawing.Size(120, 22)
         Me.DomainUpDown1.TabIndex = 3
@@ -202,7 +202,7 @@ Partial Class Form1
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(82, 34)
         Me.Button1.TabIndex = 4
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Calcular"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
@@ -211,7 +211,7 @@ Partial Class Form1
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(82, 34)
         Me.Button2.TabIndex = 5
-        Me.Button2.Text = "Button2"
+        Me.Button2.Text = "Limpiar"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button3
@@ -220,7 +220,7 @@ Partial Class Form1
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(82, 34)
         Me.Button3.TabIndex = 6
-        Me.Button3.Text = "Button3"
+        Me.Button3.Text = "Salir"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Form1
@@ -237,7 +237,7 @@ Partial Class Form1
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Salario"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
         Me.GroupBox2.ResumeLayout(False)
