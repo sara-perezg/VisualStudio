d75127d5e52b57fb3b808c1ecc2d84875e70e000
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2022-01-27 20:14:10 +0100

    all panels together :)

--- Ejercicio2-03/Ejercicio2-03/Form1.Designer.vb
+++ Ejercicio2-03/Ejercicio2-03/Form1.Designer.vb
@@ -23,44 +23,136 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.panelLateral = New System.Windows.Forms.Panel()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.panelSubmenuMedia2 = New System.Windows.Forms.Panel()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
         Me.panelSubmenuMedia = New System.Windows.Forms.Panel()
         Me.Button5 = New System.Windows.Forms.Button()
         Me.Button6 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.btnMedia = New System.Windows.Forms.Button()
         Me.panelLogo = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.panelPlayer = New System.Windows.Forms.Panel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
         Me.panelLateral.SuspendLayout()
+        Me.panelSubmenuMedia2.SuspendLayout()
         Me.panelSubmenuMedia.SuspendLayout()
+        Me.panelLogo.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'panelLateral
         '
+        Me.panelLateral.AutoScroll = True
         Me.panelLateral.BackColor = System.Drawing.Color.FromArgb(CType(CType(11, Byte), Integer), CType(CType(7, Byte), Integer), CType(CType(17, Byte), Integer))
+        Me.panelLateral.Controls.Add(Me.Panel1)
+        Me.panelLateral.Controls.Add(Me.Button7)
+        Me.panelLateral.Controls.Add(Me.panelSubmenuMedia2)
+        Me.panelLateral.Controls.Add(Me.Button4)
         Me.panelLateral.Controls.Add(Me.panelSubmenuMedia)
         Me.panelLateral.Controls.Add(Me.btnMedia)
         Me.panelLateral.Controls.Add(Me.panelLogo)
         Me.panelLateral.Dock = System.Windows.Forms.DockStyle.Left
         Me.panelLateral.Location = New System.Drawing.Point(0, 0)
         Me.panelLateral.Name = "panelLateral"
-        Me.panelLateral.Size = New System.Drawing.Size(200, 450)
+        Me.panelLateral.Size = New System.Drawing.Size(200, 553)
         Me.panelLateral.TabIndex = 0
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.FromArgb(CType(CType(5, Byte), Integer), CType(CType(32, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.Button7.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Button7.FlatAppearance.BorderSize = 0
+        Me.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button7.ForeColor = System.Drawing.Color.Gainsboro
+        Me.Button7.Location = New System.Drawing.Point(0, 478)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Padding = New System.Windows.Forms.Padding(10, 0, 0, 0)
+        Me.Button7.Size = New System.Drawing.Size(179, 45)
+        Me.Button7.TabIndex = 5
+        Me.Button7.Text = "Button1"
+        Me.Button7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'panelSubmenuMedia2
+        '
+        Me.panelSubmenuMedia2.Controls.Add(Me.Button8)
+        Me.panelSubmenuMedia2.Controls.Add(Me.Button9)
+        Me.panelSubmenuMedia2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.panelSubmenuMedia2.Location = New System.Drawing.Point(0, 395)
+        Me.panelSubmenuMedia2.Name = "panelSubmenuMedia2"
+        Me.panelSubmenuMedia2.Size = New System.Drawing.Size(179, 83)
+        Me.panelSubmenuMedia2.TabIndex = 4
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.FromArgb(CType(CType(5, Byte), Integer), CType(CType(32, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.Button8.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Button8.FlatAppearance.BorderSize = 0
+        Me.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button8.ForeColor = System.Drawing.Color.Gainsboro
+        Me.Button8.Location = New System.Drawing.Point(0, 40)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Padding = New System.Windows.Forms.Padding(35, 0, 0, 0)
+        Me.Button8.Size = New System.Drawing.Size(179, 40)
+        Me.Button8.TabIndex = 4
+        Me.Button8.Text = "Button8"
+        Me.Button8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.FromArgb(CType(CType(5, Byte), Integer), CType(CType(32, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.Button9.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Button9.FlatAppearance.BorderSize = 0
+        Me.Button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button9.ForeColor = System.Drawing.Color.Gainsboro
+        Me.Button9.Location = New System.Drawing.Point(0, 0)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Padding = New System.Windows.Forms.Padding(35, 0, 0, 0)
+        Me.Button9.Size = New System.Drawing.Size(179, 40)
+        Me.Button9.TabIndex = 3
+        Me.Button9.Text = "Button9"
+        Me.Button9.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.FromArgb(CType(CType(5, Byte), Integer), CType(CType(32, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.Button4.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Button4.FlatAppearance.BorderSize = 0
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.ForeColor = System.Drawing.Color.Gainsboro
+        Me.Button4.Location = New System.Drawing.Point(0, 350)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Padding = New System.Windows.Forms.Padding(10, 0, 0, 0)
+        Me.Button4.Size = New System.Drawing.Size(179, 45)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Button1"
+        Me.Button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button4.UseVisualStyleBackColor = False
         '
         'panelSubmenuMedia
         '
         Me.panelSubmenuMedia.BackColor = System.Drawing.Color.FromArgb(CType(CType(5, Byte), Integer), CType(CType(32, Byte), Integer), CType(CType(39, Byte), Integer))
         Me.panelSubmenuMedia.Controls.Add(Me.Button5)
         Me.panelSubmenuMedia.Controls.Add(Me.Button6)
-        Me.panelSubmenuMedia.Controls.Add(Me.Button4)
         Me.panelSubmenuMedia.Controls.Add(Me.Button3)
         Me.panelSubmenuMedia.Controls.Add(Me.Button2)
         Me.panelSubmenuMedia.Controls.Add(Me.Button1)
         Me.panelSubmenuMedia.Dock = System.Windows.Forms.DockStyle.Top
         Me.panelSubmenuMedia.Location = New System.Drawing.Point(0, 145)
         Me.panelSubmenuMedia.Name = "panelSubmenuMedia"
-        Me.panelSubmenuMedia.Size = New System.Drawing.Size(200, 250)
+        Me.panelSubmenuMedia.Size = New System.Drawing.Size(179, 205)
         Me.panelSubmenuMedia.TabIndex = 2
         '
         'Button5
@@ -69,10 +161,10 @@ Partial Class Form1
         Me.Button5.FlatAppearance.BorderSize = 0
         Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button5.ForeColor = System.Drawing.Color.Gainsboro
-        Me.Button5.Location = New System.Drawing.Point(0, 205)
+        Me.Button5.Location = New System.Drawing.Point(0, 160)
         Me.Button5.Name = "Button5"
         Me.Button5.Padding = New System.Windows.Forms.Padding(35, 0, 0, 0)
-        Me.Button5.Size = New System.Drawing.Size(200, 40)
+        Me.Button5.Size = New System.Drawing.Size(179, 40)
         Me.Button5.TabIndex = 5
         Me.Button5.Text = "Button5"
         Me.Button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
@@ -84,29 +176,14 @@ Partial Class Form1
         Me.Button6.FlatAppearance.BorderSize = 0
         Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button6.ForeColor = System.Drawing.Color.Gainsboro
-        Me.Button6.Location = New System.Drawing.Point(0, 165)
+        Me.Button6.Location = New System.Drawing.Point(0, 120)
         Me.Button6.Name = "Button6"
         Me.Button6.Padding = New System.Windows.Forms.Padding(35, 0, 0, 0)
-        Me.Button6.Size = New System.Drawing.Size(200, 40)
+        Me.Button6.Size = New System.Drawing.Size(179, 40)
         Me.Button6.TabIndex = 4
         Me.Button6.Text = "Button6"
         Me.Button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         Me.Button6.UseVisualStyleBackColor = True
-        '
-        'Button4
-        '
-        Me.Button4.Dock = System.Windows.Forms.DockStyle.Top
-        Me.Button4.FlatAppearance.BorderSize = 0
-        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button4.ForeColor = System.Drawing.Color.Gainsboro
-        Me.Button4.Location = New System.Drawing.Point(0, 120)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Padding = New System.Windows.Forms.Padding(10, 0, 0, 0)
-        Me.Button4.Size = New System.Drawing.Size(200, 45)
-        Me.Button4.TabIndex = 3
-        Me.Button4.Text = "Button1"
-        Me.Button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.Button4.UseVisualStyleBackColor = True
         '
         'Button3
         '
@@ -117,7 +194,7 @@ Partial Class Form1
         Me.Button3.Location = New System.Drawing.Point(0, 80)
         Me.Button3.Name = "Button3"
         Me.Button3.Padding = New System.Windows.Forms.Padding(35, 0, 0, 0)
-        Me.Button3.Size = New System.Drawing.Size(200, 40)
+        Me.Button3.Size = New System.Drawing.Size(179, 40)
         Me.Button3.TabIndex = 2
         Me.Button3.Text = "Button3"
         Me.Button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
@@ -132,7 +209,7 @@ Partial Class Form1
         Me.Button2.Location = New System.Drawing.Point(0, 40)
         Me.Button2.Name = "Button2"
         Me.Button2.Padding = New System.Windows.Forms.Padding(35, 0, 0, 0)
-        Me.Button2.Size = New System.Drawing.Size(200, 40)
+        Me.Button2.Size = New System.Drawing.Size(179, 40)
         Me.Button2.TabIndex = 1
         Me.Button2.Text = "Button2"
         Me.Button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
@@ -147,7 +224,7 @@ Partial Class Form1
         Me.Button1.Location = New System.Drawing.Point(0, 0)
         Me.Button1.Name = "Button1"
         Me.Button1.Padding = New System.Windows.Forms.Padding(35, 0, 0, 0)
-        Me.Button1.Size = New System.Drawing.Size(200, 40)
+        Me.Button1.Size = New System.Drawing.Size(179, 40)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Button1"
         Me.Button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
@@ -163,7 +240,7 @@ Partial Class Form1
         Me.btnMedia.Location = New System.Drawing.Point(0, 100)
         Me.btnMedia.Name = "btnMedia"
         Me.btnMedia.Padding = New System.Windows.Forms.Padding(10, 0, 0, 0)
-        Me.btnMedia.Size = New System.Drawing.Size(200, 45)
+        Me.btnMedia.Size = New System.Drawing.Size(179, 45)
         Me.btnMedia.TabIndex = 1
         Me.btnMedia.Text = "Button1"
         Me.btnMedia.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
@@ -171,22 +248,102 @@ Partial Class Form1
         '
         'panelLogo
         '
+        Me.panelLogo.Controls.Add(Me.PictureBox1)
         Me.panelLogo.Dock = System.Windows.Forms.DockStyle.Top
         Me.panelLogo.Location = New System.Drawing.Point(0, 0)
         Me.panelLogo.Name = "panelLogo"
-        Me.panelLogo.Size = New System.Drawing.Size(200, 100)
+        Me.panelLogo.Size = New System.Drawing.Size(179, 100)
         Me.panelLogo.TabIndex = 0
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PictureBox1.Image = Global.Ejercicio2_03.My.Resources.Resources.img1
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(179, 100)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'panelPlayer
+        '
+        Me.panelPlayer.BackColor = System.Drawing.Color.FromArgb(CType(CType(3, Byte), Integer), CType(CType(21, Byte), Integer), CType(CType(32, Byte), Integer))
+        Me.panelPlayer.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.panelPlayer.Location = New System.Drawing.Point(200, 453)
+        Me.panelPlayer.Name = "panelPlayer"
+        Me.panelPlayer.Size = New System.Drawing.Size(732, 100)
+        Me.panelPlayer.TabIndex = 1
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button11)
+        Me.Panel1.Controls.Add(Me.Button10)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 523)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(179, 100)
+        Me.Panel1.TabIndex = 6
+        '
+        'Button10
+        '
+        Me.Button10.BackColor = System.Drawing.Color.FromArgb(CType(CType(5, Byte), Integer), CType(CType(32, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.Button10.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Button10.FlatAppearance.BorderSize = 0
+        Me.Button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button10.ForeColor = System.Drawing.Color.Gainsboro
+        Me.Button10.Location = New System.Drawing.Point(0, 0)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Padding = New System.Windows.Forms.Padding(35, 0, 0, 0)
+        Me.Button10.Size = New System.Drawing.Size(179, 40)
+        Me.Button10.TabIndex = 4
+        Me.Button10.Text = "Button10"
+        Me.Button10.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button10.UseVisualStyleBackColor = False
+        '
+        'Button11
+        '
+        Me.Button11.BackColor = System.Drawing.Color.FromArgb(CType(CType(5, Byte), Integer), CType(CType(32, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.Button11.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Button11.FlatAppearance.BorderSize = 0
+        Me.Button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button11.ForeColor = System.Drawing.Color.Gainsboro
+        Me.Button11.Location = New System.Drawing.Point(0, 40)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Padding = New System.Windows.Forms.Padding(35, 0, 0, 0)
+        Me.Button11.Size = New System.Drawing.Size(179, 40)
+        Me.Button11.TabIndex = 5
+        Me.Button11.Text = "Button11"
+        Me.Button11.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button11.UseVisualStyleBackColor = False
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(2, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(45, Byte), Integer))
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel2.Location = New System.Drawing.Point(200, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(732, 453)
+        Me.Panel2.TabIndex = 2
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(932, 553)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.panelPlayer)
         Me.Controls.Add(Me.panelLateral)
+        Me.MinimumSize = New System.Drawing.Size(950, 600)
         Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
         Me.panelLateral.ResumeLayout(False)
+        Me.panelSubmenuMedia2.ResumeLayout(False)
         Me.panelSubmenuMedia.ResumeLayout(False)
+        Me.panelLogo.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -200,5 +357,15 @@ Partial Class Form1
     Friend WithEvents panelLogo As Panel
     Friend WithEvents Button5 As Button
     Friend WithEvents Button6 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents panelSubmenuMedia2 As Panel
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
     Friend WithEvents Button4 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents panelPlayer As Panel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Panel2 As Panel
 End Class
