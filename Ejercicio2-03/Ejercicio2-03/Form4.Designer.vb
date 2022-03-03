42fe6160bb47ac31e7806109743315ab0cb7dcca
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2022-03-03 19:44:31 +0100

    fin

---
+++ Ejercicio2-03/Ejercicio2-03/Form4.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form4
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.btnImportar = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(41, 44)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(727, 308)
+        Me.ListBox1.TabIndex = 0
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(122, 372)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(84, 54)
+        Me.btnBuscar.TabIndex = 1
+        Me.btnBuscar.Text = "Buscar musica"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(282, 372)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(84, 54)
+        Me.btnGuardar.TabIndex = 2
+        Me.btnGuardar.Text = "Guardar Musica"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'btnImportar
+        '
+        Me.btnImportar.Location = New System.Drawing.Point(442, 372)
+        Me.btnImportar.Name = "btnImportar"
+        Me.btnImportar.Size = New System.Drawing.Size(84, 54)
+        Me.btnImportar.TabIndex = 3
+        Me.btnImportar.Text = "Importar musica"
+        Me.btnImportar.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(599, 372)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(84, 54)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Salir"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form4
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(640, 360)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnImportar)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form4"
+        Me.Text = "Form4"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents btnImportar As Button
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Ejercicio2-03/Ejercicio2-03/Form4.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form4
+    Dim ofd As New OpenFileDialog
+    Dim ruta As String
+    Sub abrir()
+        ofd.Filter = "Musica en MP3 | *.mp3;"
+        If ofd.ShowDialog = Windows.Forms.DialogResult.OK Then
+            ruta = ofd.FileName
+            Form1.AxWindowsMediaPlayer1.URL = ruta
+            Form1.AxWindowsMediaPlayer1.Ctlcontrols.play()
+            ListBox1.Items.Add(ruta)
+        End If
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        abrir()
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        Form1.AxWindowsMediaPlayer1.URL = ListBox1.SelectedItem.ToString
+        Form1.AxWindowsMediaPlayer1.Ctlcontrols.play()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        Dim save As New SaveFileDialog
+        save.Filter = "Archivo de texto | *.txt"
+        If save.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Dim sw As New System.IO.StreamWriter(save.FileName)
+            For Each item In ListBox1.Items
+                sw.WriteLine(item.ToString)
+            Next
+            sw.Close()
+        End If
+    End Sub
+
+    Private Sub btnImportar_Click(sender As Object, e As EventArgs) Handles btnImportar.Click
+        Dim stRuta As String = ""
+        Dim openFD As New OpenFileDialog()
+        With openFD
+            .Title = "Seleccionar archivos"
+            .Filter = "Archivos Text(*.Text*.txt|*.Text;*.txt|Todos los archivos(*.*)|*.*)"
+            .Multiselect = False
+            .InitialDirectory = My.Computer.FileSystem.SpecialDirectories.MyMusic
+            If .ShowDialog = Windows.Forms.DialogResult.OK Then
+                stRuta = .FileName
+            End If
+        End With
+        Dim sr As New System.IO.StreamReader(stRuta)
+        While Not sr.EndOfStream
+            ListBox1.Items.Add(sr.ReadLine())
+        End While
+        sr.Close()
+    End Sub
+End Class
